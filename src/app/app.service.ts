f23c55e63854edff9a9330e4d093c552cd979fc8
chsami <sami.chkhachkhi@gmail.com>
2024-02-24 14:50:28 +0100

    added environment file for api
    added environment file for api

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient, HttpErrorResponse} from '@angular/common/http';
 import {catchError, throwError} from "rxjs";
 import {MessageService} from "primeng/api";
-
+import {environment} from "../environment/environment";
 @Injectable()
 export class AppService {
   constructor(private http: HttpClient, private messageService: MessageService) { }
@@ -24,7 +24,7 @@ export class AppService {
   }
 
   getToken() {
-    return this.http.get<string>('http://localhost:5029/token').pipe(
+    return this.http.get<string>(environment.api +'/token').pipe(
       catchError((err) => this.handleError(err))
     );
   }
